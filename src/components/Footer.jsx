a202b1435795d02a30d53c6cce1b1c1ce3f37109
Charles <Charles.liwananda@gmail.com>
2026-02-19 14:48:24 -0700

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ function Footer() {
   return (
     <footer className="footer-responsive">
       <div className="footer-content">
-        <span className="footer-logo" aria-label="Booknote logo" role="img">📚</span>
         <span className="footer-text">Copyright ⓒ {year} &nbsp;|&nbsp; Charles Liwa</span>
       </div>
     </footer>
